4b707a91fe337c43526aa454cc6f29cb3ea5934b
Gwenael <gwenael.begot@outlook.fr>
2024-04-05 12:28:54 +0200

    new ddb

--- Backend/database.sql
+++ Backend/database.sql
@@ -1,33 +1,3 @@
--- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
---
--- Host: localhost    Database: seido
--- ------------------------------------------------------
--- Server version       8.0.36
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Current Database: `seido`
---
-
-/*!40000 DROP DATABASE IF EXISTS `seido`*/;
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `seido` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-
-USE `seido`;
-
---
--- Table structure for table `advert`
---
 
 DROP TABLE IF EXISTS `advert`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -37,9 +7,9 @@ CREATE TABLE `advert` (
   `Name` varchar(60) NOT NULL,
   `Price` decimal(10,0) NOT NULL,
   `NameShoes` varchar(45) NOT NULL,
-  `Picture` varchar(400) NOT NULL,
+  `Picture` varchar(400) DEFAULT NULL,
   PRIMARY KEY (`idAdvert`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -48,17 +18,6 @@ CREATE TABLE `advert` (
 
 LOCK TABLES `advert` WRITE;
 /*!40000 ALTER TABLE `advert` DISABLE KEYS */;
-INSERT INTO `advert` VALUES (1,'AIR FORCE ONE',200,'MURASAKI','pict'),(2,'AIR FORCE ONE',150,'NIHON','pict'),(3,'Air jordan retro 4',300,'Nami','pict');
+INSERT INTO `advert` VALUES (1,'Air force one',200,'Murasaki','/static/logoProj.png'),(30,'Air force one',200,'Murasaki','/static/logoProj.png'),(31,'Air force one',200,'Murasaki','/static/logoProj.png'),(32,'Air force one',200,'Murasaki','/static/logoProj.png'),(33,'Air force one',200,'Murasaki','/static/logoProj.png'),(34,'Air force one',200,'Murasaki','/static/logoProj.png'),(37,'Air force one',220,'A-T','/static/logoShoesA-T.png'),(38,'Air force one',220,'A-T','/static/logoShoesA-T.png'),(39,'Air force one',220,'A-T','/static/logoShoesA-T.png'),(40,'Air force one',220,'A-T','/static/logoShoesA-T.png'),(41,'Air force one',220,'A-T','/static/logoShoesA-T.png'),(42,'Air force one',220,'A-T','/static/logoShoesA-T.png');
 /*!40000 ALTER TABLE `advert` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-04-03 12:54:02
